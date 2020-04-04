1b2bd3a23af8cb18b70d32e01e0e466f10cb53ca
Aristotelis Dossas <teldosas@gmail.com>
2020-04-04 18:31:00 +0300

    Init...

---
+++ client.js
@@ -0,0 +1,47 @@
+const $ = require('jquery');
+require('bootstrap');
+const _ = require('underscore');
+global.jQuery = $;
+require('bootstrap-input-spinner');
+
+var socket = io();
+var numOfPlayers;
+
+socket.on('roles', (msg) => {
+  document.querySelector('#role').innerHTML = msg;
+});
+
+socket.on('players-change', (msg) => {
+  document.querySelector('#numOfPlayers').innerHTML = 'Παίκτες: ' + msg;
+  numOfPlayers = msg;
+});
+$('input[type=number]').inputSpinner()
+
+const getNums = () => {
+  const nums = {};
+
+  const rawNums = $('input[type=number]').toArray().map(e => [e.getAttribute('id'), e.value]);
+  rawNums.forEach(n => nums[n[0]] = parseInt(n[1]));
+  return nums;
+}
+
+$('form').submit(e => {
+  e.preventDefault();
+  socket.emit('deal', getNums());
+});
+
+socket.on('num-change', (msg) => {
+  $(`#${msg[0]}`).val(msg[1]);
+});
+
+var callBackId;
+$('input[type=number]').change(e => {
+  const numOfRoles = _.values(getNums()).reduce((acc, e) => acc+e, 0);
+  const diff = numOfPlayers - numOfRoles
+  if (diff < 0) {
+    $(e.target).val(parseInt(e.target.value) + diff);
+    return;
+  }
+  clearTimeout(callBackId);
+  socket.emit('num-change', [e.target.getAttribute('id'), e.target.value])
+})
---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const _ = require('underscore');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + 'public/index.html');
+});
+
+io.on('connection', socket => {
+  const numOfPlayers = Object.keys(io.sockets.sockets).length;
+  console.log('a user connected ' + numOfPlayers);
+
+  io.emit('players-change', numOfPlayers);
+
+  socket.on('num-change', msg => {
+    // WARNING!!! XSS vulnerability
+    io.emit('num-change', msg);
+  });
+
+  socket.on('deal', ({numOfSecretMurderers, numOfKnownMurderers, numOfDetectives, numOfHealers, numOfBigBosses}) => {
+    const nums = [numOfSecretMurderers, numOfKnownMurderers, numOfDetectives, numOfHealers, numOfBigBosses];
+    const roleNames = ['Κρυφός Δολοφόνος', 'Φανερός Δολοφόνος', 'Ντετέκτιβ', 'EKAB', 'Μεγάλο Αφεντικό', 'Πολίτης'];
+    const socketIds = Object.keys(io.sockets.sockets);
+    const numOfSpecialRoles = nums.reduce((sum, n) => sum + n, 0);
+    const numOfCitizens = socketIds.length - numOfSpecialRoles;
+
+    if (numOfCitizens < 0) {
+      io.emit('roles', '!!! Πολλοι ρόλοι');
+      return;
+    }
+
+    nums.push(numOfCitizens);
+
+    const roles = [];
+    roleNames.forEach((role, i) => {
+      Array.prototype.push.apply(roles, Array(nums[i]).fill(role));
+    });
+
+    const shuffledRoles = _.shuffle(roles);
+    _.shuffle(socketIds).forEach((socketId, i) => {
+      io.to(socketId).emit('roles', shuffledRoles[i]);
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+      const numOfPlayers = Object.keys(io.sockets.sockets).length;
+      io.emit('players-change', numOfPlayers);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
